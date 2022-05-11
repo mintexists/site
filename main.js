3a35c20e69b960da4195d87145fde59e0c90ca25
evaexists <bates.nevin@doversherborn.org>
2022-05-11 15:09:47 -0400

    done

---
+++ main.js
@@ -0,0 +1,19 @@
+let discordButton = document.getElementById("discord-button")
+
+let clicked = false;
+
+discordButton.addEventListener("click", e => {
+    if (!clicked) {
+        clicked = true;
+    } else {
+        navigator.permissions.query({name: "clipboard-write"}).then(result => {
+            if (result.state == "granted" || result.state == "prompt") {
+                navigator.clipboard.writeText("evalyn#8883").then(function() {
+                    console.log("Copied!");
+                }, function() {
+                    console.log("Not copied!");
+                });
+            }
+        });
+    }
+})
