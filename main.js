dfbe51c6f9d0ebdad13981d0ca3ea862e6d184ba
autumn <60362123+enbyautumn@users.noreply.github.com>
2022-07-13 16:15:39 -0400

    bop boop make it sexier

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ discordButton.addEventListener("click", e => {
         });
     }
 })
+
+let mapRange = (value, low1, high1, low2, high2) => low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+
+let inter = (x, a) => (x ** a) / ((x ** a) + ((1 - x) ** a));
+
+let bar = document.getElementById("bar")
+
+document.addEventListener("mousemove", e => {
+    let centerX = bar.offsetLeft - bar.offsetWidth;
+    let centerY = bar.offsetTop//  + bar.offsetHeight / 2;
+
+    let xOffset = e.clientX - centerX;
+    let yOffset = e.clientY - centerY;
+
+    let mappedX = 0;
+
+    mappedX = mapRange(e.clientX - (.20 * document.body.offsetWidth), 0, document.body.offsetWidth, 0, 1);
+
+    let smoothed = inter(Math.abs(mappedX), 1.2) * Math.sign(mappedX);
+
+    bar.style.setProperty('--offset', `${smoothed * 50}px`);
+})
